2f8f79bd3590620a58b46c10c59c7c92586d4d11
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-08-30 17:07:26 -0500

    placeholder; browser management

--- src/bit_browser/models/manager/__init__.py
+++ src/bit_browser/models/manager/__init__.py
@@ -1,5 +1,31 @@
-from browser import Browser
+from dataclasses import dataclass
+from enum import Enum
+
+from bit_browser.models import client
+from bit_browser.models.browser import Browser
+
+
+class Status(Enum):
+    open = "open"
+    closed = "closed"
+
+
+@dataclass
+class Session:
+    id: str
+    status: Status
+    browser: Browser
 
 
 class BrowserManager:
-    pass
+    def __init__(self):
+        self.browsers: list[Session] = []
+        self.client = client
+
+    def _helper(self):
+        self._get_all_browsers()
+
+    def _get_all_browsers(self) -> None:
+        """_summary_"""
+        o: list[Session] = []
+        pass
